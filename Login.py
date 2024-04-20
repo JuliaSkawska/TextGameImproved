05e9dd7bfa96e7ce36de89fd14191b09f6d9657e
JuliaSkawska <101816101+JuliaSkawska@users.noreply.github.com>
2024-04-20 16:29:35 +0200

    Update Login
    Updated Login file to also return username for easier connection to a database for the rest of the game
    Improved code quality

--- Login.py
+++ Login.py
@@ -1,6 +1,9 @@
 import mysql.connector
 
-class Login:
+class LoginRegister:
+    '''
+        Allows user to login or register, as well as add that user to a database
+    '''
     def __init__(self):
         self.conn = mysql.connector.connect(
             host="localhost",
@@ -8,51 +11,51 @@ class Login:
             password="password",
             database="UserLogin"
         )
-        correct=False
-        while not correct:
-            choice=input("Would you like to login or register?: ")
-            choice=choice.strip().lower()
-            if choice=="login":
-                self.login()
-                correct=True
-            elif choice=="register":
-                self.register()
-                correct=True
+
+        while True:
+            choice = input("Would you like to login or register?: ").strip().lower()
+            if choice == "login":
+                self.username = self.login()
+                if self.username:
+                    break
+            elif choice == "register":
+                self.username = self.register()
+                break
 
     def login(self):
-        found = False
         print("\nWelcome, let's try to log you in :D\n")
 
-        while not found:
-            logusr = input("Enter your username: ")
-            passusr = input("Enter your password: ")
+        while True:
+            username = input("Enter your username: ")
+            password = input("Enter your password: ")
 
             cursor = self.conn.cursor()
-            cursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (logusr, passusr))
-            row = cursor.fetchone()
-
-            if row:
-                found = True
+            cursor.execute("SELECT username FROM users WHERE username = %s AND password = %s", (username, password))
+            user = cursor.fetchone()
+            if user:
                 print("User found... logging in")
+                return username
             else:
                 print("Login or password incorrect, please try again:")
+        return None
+
     def register(self):
-        checkpass=False
         print("\nWelcome, let's create your new account\n")
-        new_usr = input("Enter a new username: ")
-        new_pass = input("Enter a new password: ")
+        new_username = input("Enter a new username: ")
+        new_password = input("Enter a new password: ")
 
-        while not checkpass:
-            new_pass2 = input("Enter your new password again: ")
+        while True:
+            new_password_confirmation = input("Enter your new password again: ")
 
-            if new_pass==new_pass2:
+            if new_password == new_password_confirmation:
                 cursor = self.conn.cursor()
-                cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (new_usr, new_pass))
+                cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (new_username, new_password))
                 self.conn.commit()
-                print("Your account has been added!")
-                checkpass=True
+                print("Your account has been created!\n")
+                return self.login()
             else:
-                print("Whoops, looks like something went wrong, make sure your password matches")
-        self.login()
+                print("Whoops, looks like something went wrong. Make sure your password matches.\n")
+        return None
 
-p1 = Login()
+p=LoginRegister()
+print(p.username)
