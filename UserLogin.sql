e6d8037377414d60974f31d11bbc01e34c888a5d
JuliaSkawska <101816101+JuliaSkawska@users.noreply.github.com>
2024-04-22 16:20:31 +0200

    Update UserLogin.sql
    Added an option to save characters
    Added a view to see users and their characters

--- UserLogin.sql
+++ UserLogin.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS mydb;
+CREATE DATABASE IF NOT EXISTS userlogin;
 
-USE mydb;
+USE userlogin;
 
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,12 +8,30 @@ CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS playercharacter (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    charname VARCHAR(255) NOT NULL,
+    charhealth INT NOT NULL,
+    profession VARCHAR(10) NOT NULL,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
 INSERT INTO users (username, password) VALUES
 ('user1', 'password1'),
 ('user2', 'password2'),
 ('user3', 'password3');
 
-CREATE VIEW user_view AS
-SELECT * FROM users;
+INSERT INTO playercharacter (charname, charhealth, profession, user_id) VALUES
+('Prettyelf', 100, 'Thief', 1),
+('Mario', 80, 'Alchemist', 2),
+('Test1', 80, 'Thief', 3),
+('Test2', 90, 'Warrior', 3),
+('Test3', 70, 'Mage', 3);
 
-SELECT*FROM user_view
+CREATE VIEW user_characters AS
+SELECT u.username, pc.charname, pc.charhealth, pc.profession
+FROM users AS u
+JOIN playercharacter AS pc ON u.id = pc.user_id;
+
+SELECT * FROM user_characters;
